44093d5800f5109862fd466e830b3d0540c6161a
irem acar <iremmacar35@gmail.com>
2024-07-03 12:47:29 +0300

    commit

---
+++ src/components/Contacts/index.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import From from './Form/index';
+import List from './List/index';
+
+function Contacts() {
+    const [contacts, setContacts] = useState([]);
+
+    const addContact = (newContact) => {
+        setContacts([...contacts, newContact]);
+    };
+
+    return (
+        <div>
+            <List contacts={contacts} />
+            <From addContact={addContact} />
+        </div>
+    );
+}
+
+export default Contacts;
