08d1a7eb4cab62e8d4d5b243b369b06157257285
irem acar <iremmacar35@gmail.com>
2024-07-06 15:15:37 +0300

    filter commit

--- src/components/Contacts/List/index.js
+++ src/components/Contacts/List/index.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function List({ contacts }) {
+  const [filterText,setFilterText]=useState('');
+
+  const filtered = contacts.filter((item)=>{
+    return Object.keys(item).some((key)=>
+      item[key].toString().toLowerCase().includes(filterText.toLocaleLowerCase())
+    )
+  })
+
+  console.log("filtered",filtered);
   return (
     <div>
+
+      <input placeholder='Filter'
+      value={filterText}
+      onChange={(e)=>setFilterText(e.target.value)}/>
+
       <ul>
-        {contacts.map((contact, i) => (
+        {filtered.map((contact, i) => (
           <li key={i}>{contact.fullname}</li>
         ))}
       </ul>
--- src/components/Contacts/index.js
+++ src/components/Contacts/index.js
@@ -3,7 +3,20 @@ import Form from './Form';
 import List from './List'; 
 
 function Contacts() {
-  const [contacts, setContacts] = useState([]);
+  const [contacts, setContacts] = useState([
+    {
+        fullname:'Irem',
+        phoneNumber:'2006'
+    },
+    {
+        fullname:'Ibrahim',
+        phoneNumber:'2022'
+    },
+    {
+        fullname:'Hilal',
+        phoneNumber:'2018'
+    }
+  ]);
 
   const addContact = (newContact) => {
     setContacts([...contacts, newContact]);
